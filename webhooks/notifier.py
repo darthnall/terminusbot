d66875e059e0c50c8a4f988ff7d41b2ede809782
Blake Nall <blake@proton.me>
2024-05-01 11:38:55 -0500

    Cleaned up code

--- webhooks/notifier.py
+++ webhooks/notifier.py
@@ -31,14 +31,12 @@ class PhoneNotifier:
         return None
 
     def notify(self, phone: str, msg: str) -> None:
-        try:
-            self.caller.send(phone, msg)
-        except Exception:
-            return None
+        self.caller.send(phone, msg)
+
+        return None
 
     def batch_notify(self, phones: list[str], msg: str) -> None:
-        try:
-            for phone in phones:
-                self.notify(phone, msg)
-        except Exception:
-            return None
+        for phone in phones:
+            self.notify(phone, msg)
+
+        return None
