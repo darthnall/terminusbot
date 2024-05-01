ec6452845942255372d41abe3635fb59766195ca
Blake Nall <blake@proton.me>
2024-05-01 14:21:11 -0500

    Started impl

--- user/create_user.py
+++ user/create_user.py
@@ -14,14 +14,28 @@ class TerminusUser:
     is_wialon_user: bool = False
     is_video_user: bool = False
 
+    def __post_init__(self) -> None:
+        if self.is_wialon_user ^ self.is_video_user:
+            pass
+
+        return None
+
     def create_wialon_user(self, params: dict) -> None:
         with Session() as session:
-            session.wialon_api.core_create_user(**params)
-            self.is_wialon_user = True
+            response = session.wialon_api.core_create_user(**params)
+
+            if not response.status_code == "200":
+                raise WialonUserExistsError
+
+        self.is_wialon_user = True
+
+        return None
 
     def create_video_user(self, params: dict) -> None:
         print("Creating video user...")
         print("Created video user.")
+
+        return None
 
     @property
     def fullname(self) -> str:
