6984c7368079c2af2ff80da2f6fdb95c14da4175
Blake Nall <blake@proton.me>
2024-05-01 14:06:43 -0500

    Removed debugging main entrypoint

--- user/create_user.py
+++ user/create_user.py
@@ -1,6 +1,7 @@
 from auth.session import Session
-from auth.flags import WialonApiFlags as flags
 from dataclasses import dataclass
+
+from user.errors import WialonUserExistsError, VideoUserExistsError
 
 
 @dataclass
@@ -14,10 +15,9 @@ class TerminusUser:
     is_video_user: bool = False
 
     def create_wialon_user(self, params: dict) -> None:
-        if not self.is_wialon_user:
-            with Session() as session:
-                session.wialon_api.core_create_user(**params)
-                self.is_wialon_user = True
+        with Session() as session:
+            session.wialon_api.core_create_user(**params)
+            self.is_wialon_user = True
 
     def create_video_user(self, params: dict) -> None:
         print("Creating video user...")
@@ -26,17 +26,3 @@ class TerminusUser:
     @property
     def fullname(self) -> str:
         return f"{self.first} {self.last}"
-
-def main():
-    params = {}
-    user = TerminusUser(
-        first="John",
-        last="Doe",
-        email="john@doe.com",
-        phone="+17133049421",
-    ).create_wialon_user(params=params)
-    print(user.is_wialon_user)
-
-if __name__ == "__main__":
-    flags = flags.UserFlags
-    print(flags.BASE.value)
---
+++ user/errors.py
@@ -0,0 +1,5 @@
+class WialonUserExistsError(Exception):
+    """ Raised when the Terminus user already exists in Wialon. """
+
+class VideoUserExistsError(Exception):
+    """ Raised when the Terminus user already exists in Video. """
